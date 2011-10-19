b3e5afde7b7b65d4d2bd5f23460f45617c095298
Marty Schoch <marty.schoch@gmail.com>
2011-10-19 14:51:29 -0400

    updated to not build final product only contributions
    removed generated jni and obj files
    removed generated overlay.zip
    removed OtpErlang.jar it is now referenced from erlang build
    script updated to accomodate all libs being in overlay now
    dist target removed as this is done by build.final now
    
    Change-Id: I2d2244b5a9ca362dbc5616a0e8b6027b6bf6a8d5
    Reviewed-on: http://review.couchbase.org/10213
    Tested-by: Marty Schoch <marty.schoch@gmail.com>
    Reviewed-by: Marty Schoch <marty.schoch@gmail.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := com_couchbase_android_ErlangThread
 LOCAL_SRC_FILES := com_couchbase_android_ErlangThread.cpp android_jni_nif.cpp
-LOCAL_LDLIBS    := -llog -ldl -L$(LOCAL_PATH) -lbeam
+LOCAL_LDLIBS    := -llog -ldl -L$(LOCAL_PATH) -L$(ERL_HOME)/bin/arm-unknown-eabi/ -lbeam
 
 include $(BUILD_SHARED_LIBRARY)
