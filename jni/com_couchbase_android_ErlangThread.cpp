53c57cb0e2cf4b5f22977ceaf7d7f8c701bd6003
Marty Schoch <marty.schoch@gmail.com>
2011-11-08 16:23:31 -0500

    log messages sent to stdout/stderr before the VM is booted
    Change-Id: I604135c8ccd414cbd0500254a0500182a58259c4
    Reviewed-on: http://review.couchbase.org/10702
    Tested-by: Marty Schoch <marty.schoch@gmail.com>
    Reviewed-by: Marty Schoch <marty.schoch@gmail.com>

--- jni/com_couchbase_android_ErlangThread.cpp
+++ jni/com_couchbase_android_ErlangThread.cpp
@@ -6,9 +6,88 @@
 
 #include "android/log.h"
 
+#include "erl_printf.h"
+
 #define LOG_TAG "ErlangThread"
 #define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#define ALLOC(X) malloc(X)
+#define REALLOC(X,Y) realloc(X,Y)
+#define FREE(X) free(X)
+
+#define ANDROID_VPRINTF_BUF_INC_SIZE 1024
+
+int (*ref_erts_vdsprintf)(erts_dsprintf_buf_t *dsbufp, const char *format, va_list arglist) = NULL;
+
+static erts_dsprintf_buf_t * grow_android_vprintf_buf(erts_dsprintf_buf_t *dsbufp, size_t need)
+{
+    char *buf;
+    size_t size;
+
+    if (!dsbufp->str) {
+        size = (((need + ANDROID_VPRINTF_BUF_INC_SIZE - 1)
+                 / ANDROID_VPRINTF_BUF_INC_SIZE)
+                * ANDROID_VPRINTF_BUF_INC_SIZE);
+        buf = (char *) ALLOC(size * sizeof(char));
+    }
+    else {
+        size_t free_size = dsbufp->size - dsbufp->str_len;
+
+        if (need <= free_size)
+            return dsbufp;
+
+        size = need - free_size + ANDROID_VPRINTF_BUF_INC_SIZE;
+        size = (((size + ANDROID_VPRINTF_BUF_INC_SIZE - 1)
+                 / ANDROID_VPRINTF_BUF_INC_SIZE)
+                * ANDROID_VPRINTF_BUF_INC_SIZE);
+        size += dsbufp->size;
+        buf = (char *) REALLOC((void *) dsbufp->str,
+                               size * sizeof(char));
+    }
+    if (!buf)
+        return NULL;
+    if (buf != dsbufp->str)
+        dsbufp->str = buf;
+    dsbufp->size = size;
+    return dsbufp;
+}
+
+static int android_stdout_vprintf(char *format, va_list arg_list) {
+    int res,i;
+    erts_dsprintf_buf_t dsbuf = ERTS_DSPRINTF_BUF_INITER(grow_android_vprintf_buf);
+    res = ref_erts_vdsprintf(&dsbuf, format, arg_list);
+    if (res >= 0) {
+	char *tmp = (char*)ALLOC((dsbuf.str_len + 1)*sizeof(char));
+	for (i=0;i<dsbuf.str_len;++i) {
+	    tmp[i] = dsbuf.str[i];
+	}
+        tmp[dsbuf.str_len] = NULL;
+        LOGV(tmp);
+	FREE(tmp);
+    }
+    if (dsbuf.str)
+      FREE((void *) dsbuf.str);
+    return res;
+}
+
+static int android_stderr_vprintf(char *format, va_list arg_list) {
+    int res,i;
+    erts_dsprintf_buf_t dsbuf = ERTS_DSPRINTF_BUF_INITER(grow_android_vprintf_buf);
+    res = ref_erts_vdsprintf(&dsbuf, format, arg_list);
+    if (res >= 0) {
+        char *tmp = (char*)ALLOC((dsbuf.str_len + 1)*sizeof(char));
+        for (i=0;i<dsbuf.str_len;++i) {
+            tmp[i] = dsbuf.str[i];
+        }
+        tmp[dsbuf.str_len] = NULL;
+        LOGE(tmp);
+        FREE(tmp);
+    }
+    if (dsbuf.str)
+      FREE((void *) dsbuf.str);
+    return res;
+}
 
 JavaVM *jvm;
 jclass ErlangThread;
@@ -20,6 +99,8 @@ JNIEXPORT void JNICALL Java_com_couchbase_android_ErlangThread_start_1erlang
     int i, argc;
     char ** argv;
     void (*erl_start)(int, char**) = NULL;
+    int (**erts_printf_stdout_func)(char *, va_list) = NULL;
+    int (**erts_printf_stderr_func)(char *, va_list) = NULL;
 
     const char *sopath = env->GetStringUTFChars(j_sopath, &iscopy);
     const char *bindir = env->GetStringUTFChars(j_bindir, &iscopy);
@@ -35,6 +116,31 @@ JNIEXPORT void JNICALL Java_com_couchbase_android_ErlangThread_start_1erlang
         LOGE("Failed to find erl_start: %s", dlerror());
         goto cleanup;
     }
+
+    *(void **)(&erts_printf_stdout_func) = dlsym(handle, "erts_printf_stdout_func");
+    if(!erts_printf_stdout_func) {
+        LOGE("Failed to find erts_printf_stdout_func: %s", dlerror());
+        goto cleanup;
+    }
+    else {
+        *erts_printf_stdout_func = android_stdout_vprintf;
+    }
+
+    *(void **)(&erts_printf_stderr_func) = dlsym(handle, "erts_printf_stderr_func");
+    if(!erts_printf_stderr_func) {
+        LOGE("Failed to find erts_printf_stderr_func: %s", dlerror());
+        goto cleanup;
+    }
+    else {
+        *erts_printf_stderr_func = android_stderr_vprintf;
+    }
+
+    *(void **)(&ref_erts_vdsprintf) = dlsym(handle, "erts_vdsprintf");
+    if(!erts_vdsprintf) {
+        LOGE("Failed to find erts_vdsprintf: %s", dlerror());
+        goto cleanup;
+    }
+
     argc = env->GetArrayLength(j_args);
     argv = (char**) malloc(sizeof(char*) * argc);
 
